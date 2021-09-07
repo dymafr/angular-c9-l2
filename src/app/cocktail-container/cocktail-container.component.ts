b9283c61c1dec16c041efd8e0ac95b13b41010dd
dymafr <erwan1309@hotmail.com>
2021-09-07 13:55:24 +0200

    3 files modified

--- src/app/cocktail-container/cocktail-container.component.ts
+++ src/app/cocktail-container/cocktail-container.component.ts
@@ -1,21 +1,17 @@
-import { Component, OnInit } from "@angular/core";
-import { Cocktail } from "../shared/interfaces/cocktail.interface";
-import { CocktailService } from "../shared/services/cocktail.service";
+import { Component } from '@angular/core';
+import { Cocktail } from '../shared/interfaces/cocktail.interface';
+import { CocktailService } from '../shared/services/cocktail.service';
 
 @Component({
-  selector: "app-cocktail-container",
-  templateUrl: "./cocktail-container.component.html",
-  styleUrls: ["./cocktail-container.component.scss"]
+  selector: 'app-cocktail-container',
+  templateUrl: './cocktail-container.component.html',
+  styleUrls: ['./cocktail-container.component.scss']
 })
-export class CocktailContainerComponent implements OnInit {
-  public selectedCocktail: Cocktail;
-  public cocktails: Cocktail[];
+export class CocktailContainerComponent {
+  public cocktails: Cocktail[] = [];
+  public selectedCocktail: Cocktail = this.cocktails[0];
 
   constructor(private cocktailService: CocktailService) {}
-
-  ngOnInit() {
-    this.selectedCocktail = this.cocktails[0];
-  }
 
   public selectCocktail(index: number): void {
     this.selectedCocktail = this.cocktails[index];
